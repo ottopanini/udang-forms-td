e8c9933e9fad23de55cce95b9c6a6b018e8e24f0
Christian Preß <Christian.Press@syracos.com>
2020-05-24 16:12:34 +0200

    Using ngModel with Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', {static: true})
   private signupForm: NgForm;
+  questionAnswer = '';
 
   defaultQuestion = 'pet';
 
