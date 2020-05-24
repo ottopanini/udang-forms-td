530df00aad2a22d3674ef34ea031b5c9a95ae230
Christian Preß <Christian.Press@syracos.com>
2020-05-24 17:01:39 +0200

    TD: Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,16 @@ export class AppComponent {
 
   genders = ['male', 'female'];
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signupForm.setValue({
@@ -44,5 +54,12 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
+
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
