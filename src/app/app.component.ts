10ad2f93d769f1a47ece11976bf243f612ac672a
Christian Preß <Christian.Press@syracos.com>
2020-05-24 16:05:36 +0200

    TD: Set Default Values with ngModel Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   @ViewChild('f', {static: true})
   private signupForm: NgForm;
 
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
